3a7852913f5e600a27adaf37b984c174e5713903
joydip007x <joydip007x@gmail.com>
2023-02-04 02:37:31 +0600

    chartDistr ended

--- src/components/MinterDist/MinterDist.js
+++ src/components/MinterDist/MinterDist.js
@@ -5,12 +5,16 @@ import {useNavigate ,Link} from 'react-router-dom';
 import {useLocation} from 'react-router-dom';
 
 import Navbar from '../Navbar/Navbar.js'
+import { Chart } from "chart.js";
 
 
 import './MinterDist.css'
 
 
 const MinterDist = () => {
+
+  const canvas = useRef();
+  let ctx = null,ctxwidth=null,ctxheight=null;
 
   var [blockList,setblockList,blockListRef]=useState([]);
   var  [chainHeight,setChainHeight,chainHeightRef] = useState(-1);
@@ -28,7 +32,7 @@ const MinterDist = () => {
   function reSetblockList (arr){
       
     setblockList(arr);
-    console.log("reSetblockList :"+blockList.length);
+    //console.log("reSetblockList :"+blockList.length);
     // RectCanvas(arr);
   }
   const sleep = async (milliseconds) => {
@@ -64,7 +68,7 @@ const MinterDist = () => {
     reader.onload =async (event) => {
 
         let text=event.target.result;
-        console.log(text)
+        //console.log(text)
         var index=0,match,index2=0;
         let arr1 = [],arr2=[],temp=[];
         //store ending index to scrap each line
@@ -87,27 +91,135 @@ const MinterDist = () => {
               Minter:parseString(pat2,s),
               OnChain:parseString(pat4_orphan,s,1)? false:true,
             }
-            if(flowBlock.OnChain) index2++;
-            temp[index]=flowBlock;
-            index=index+1;
+            if(flowBlock.OnChain) {
+              index2++;
+              temp[index]=flowBlock.Minter;
+              index=index+1;
+            }
            // console.log(s+":"+flowBlock.OnChain+" : "+flowBlock.ChainNo+" "+flowBlock.Minter);
         }
         await sleep(2000);
         setChainHeight(index2);
-        console.log("OK--------"+chainHeightRef.current)
+        console.log("OK--------"+chainHeightRef.current+ " \n "+temp)
         reSetblockList(temp);
         //console.log(blockList.length);
       };
     reader.readAsText(data);
   
   },[]);
+  useEffect( () => {
+    // dynamically assign the width and height to canvas
 
+   async function createCanvas(){
+      while( chainHeightRef.current==-1){
+
+          await sleep(1000);
+      }
+    //   console.log("OK||"+chainHeightRef.current)
+    //   const canvasEle = canvas.current;
+    //   canvasEle.width = window.innerWidth;
+    //   canvasEle.height = window.innerHeight /*100+(120*chainHeightRef.current)*/;
+      
+    //   ctxwidth= canvasEle.width;
+    //   ctxheight= canvasEle.height ;
+      // get context of the canvas
+      ctx = document.getElementById("myChart4").getContext('2d');///canvasEle.getContext("2d");
+      
+      DataOfCanvas(blockListRef.current,ctx);
+   }
+   createCanvas();
+
+    
+  }, []);
+  function DataOfCanvas(blockArray,ctx){
+    
+    let maxNode=-1,labelsMinter=[],dataMinter=[];
+    let bgColor=[],bgBorderColor=[];
+    const backgroundColor= [
+      'rgba(153, 102, 255, 0.7)',
+      'rgba(54, 162, 235, 0.7)',
+      'rgba(255, 206, 86, 0.7)',
+      'rgba(75, 192, 192, 0.7)',
+      'rgba(255, 99, 132, 0.6)',
+      'rgba(255, 159, 64, 0.8)'
+    ]
+    const borderColor= [
+      'rgba(153, 102, 255, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(255,99,132,1)',
+      'rgba(255, 159, 64, 1)'
+    ];
+    const MinterFreq = blockArray.reduce((acc, curr) => {
+      acc[curr] = (acc[curr] ?? 0) + 1;
+      maxNode=Math.max(maxNode,curr)
+      return acc;
+    }, {});
+
+    console.log('Freq : '+MinterFreq+ ": XX "+borderColor[0]);
+    for(let i=0; i<=maxNode;i++){
+       if(MinterFreq[i]){
+            console.log(i + ' = '+MinterFreq[i]);
+            labelsMinter.push('Minter '+i);
+            dataMinter.push(MinterFreq[i]);
+            if(MinterFreq[i]<=4)
+                {bgColor.push(backgroundColor[MinterFreq[i]-1]);bgBorderColor.push(borderColor[MinterFreq[i]-1]);}
+            else{bgColor.push(backgroundColor[5]);bgBorderColor.push(borderColor[5]);}
+           
+       }
+    }
+
+    ///var ctx = document.getElementById("myChart4").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labelsMinter,
+        datasets: [{
+          label: '# of Blocks Mined',
+          backgroundColor: bgColor,
+          borderColor:bgBorderColor,
+          data: dataMinter,
+          borderWidth: 2
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          },
+          x: {
+            beginAtZero: true
+          },
+          xAxes: [{ 
+            gridLines: { lineWidth: 2 },
+            zeroLineWidth:5,
+            ticks: {
+              fontSize: 15,
+             
+            }
+         }],
+         yAxes: [{ 
+            gridLines: { lineWidth: 3 },
+            ticks: {
+              beginAtZero:true,
+              fontSize: 15,
+              fontStyle: "bold",
+            }
+         }]
+        }
+      }
+    });
+  }
   return (
     <div>
         <Navbar pageName='Minter⠀Distribution '  data={location.state?.data}
         root={'mintflow'}
         />
-        <div>MinterDist</div>
+        {/* <div>MinterDist</div> */}
+        <div class="wrapper">
+        <canvas id="myChart4"></canvas>
+        </div>
     </div>
   )
 }
